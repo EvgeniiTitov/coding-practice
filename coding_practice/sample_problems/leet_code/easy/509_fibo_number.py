d2d5256169baf3ed9a69542cb9688063f9309c40
Evgenii Titov <1994etitov@gmail.com>
2022-06-23 15:18:48 +1000

    ...

---
+++ coding_practice/sample_problems/leet_code/easy/509_fibo_number.py
@@ -0,0 +1,35 @@
+
+
+'''
+https://leetcode.com/problems/fibonacci-number/
+
+
+06/23/2022 14:05	Accepted	1333 ms	13.8 MB	python3  - without cache
+06/23/2022 14:05	Accepted	42 ms	14 MB	python3
+'''
+
+
+class Solution:
+    def fib(self, n: int) -> int:
+
+        def _cache(func):
+            cache = {}
+
+            def wrapper(n: int) -> int:
+                if n in cache:
+                    return cache[n]
+                result = func(n)
+                cache[n] = result
+                return result
+
+            return wrapper
+
+        @_cache
+        def _calculate_fibo(n: int) -> int:
+            if n == 0:
+                return 0
+            elif n == 1:
+                return 1
+            return _calculate_fibo(n - 1) + _calculate_fibo(n - 2)
+
+        return _calculate_fibo(n)
