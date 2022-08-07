5f66f6d5b6a4403003674a38705dabba48cd75cc
Evgenii Titov <1994etitov@gmail.com>
2022-08-07 22:24:25 +1000

    ...

---
+++ coding_practice/algorithms/divide_and_conquer/TO_COMPLETE_nb_paths_reach_last_cell_given_cost.py
@@ -0,0 +1,56 @@
+import typing as t
+
+
+"""
+Number of paths to reach the last cell with given cost
+
+Given 2D matrix
+Each cell has a cost associated with it for accessing
+Starting from 0,0 need to reach n-1,m-1
+We can only go down and right
+We're given the total cost to reach the last cell
+Find the total N of paths to reach the end within the given total cost
+"""
+
+
+Matrix = t.List[t.List[int]]
+
+
+def find_paths(
+    matrix: Matrix, cost: int, i: int = 0, j: int = 0
+) -> t.Union[int, float]:
+    # Base cases
+    remaining_cost = cost - matrix[i][j]
+    if remaining_cost < 0:
+        return 0
+
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    if i == rows - 1 and j == cols - 1:
+        return 1 if remaining_cost == 0 else 0
+
+    if i == rows - 1:
+        return find_paths(matrix, remaining_cost, i, j + 1)
+    elif j == cols - 1:
+        return find_paths(matrix, remaining_cost, i + 1, j)
+
+    path1 = find_paths(matrix, remaining_cost, i + 1, j)
+    path2 = find_paths(matrix, remaining_cost, i, j + 1)
+
+    return path1 + path2
+
+
+def main():
+    grid = [
+        [4, 7, 1, 6],
+        [5, 7, 3, 9],
+        [3, 2, 1, 2],
+        [7, 1, 6, 3]
+    ]
+    cost = 25
+    print(find_paths(grid, cost))
+
+
+if __name__ == '__main__':
+    main()
