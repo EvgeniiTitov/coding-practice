ec7f6308966a5dfa39f93afc67854e2a060a5ffb
Evgenii Titov <1994etitov@gmail.com>
2022-08-08 16:49:19 +1000

    ...

--- coding_practice/algorithms/dynamic/convert_one_str_to_another.py
+++ coding_practice/algorithms/dynamic/convert_one_str_to_another.py
@@ -1,4 +1,10 @@
+import typing as t
+
 from coding_practice.utils import times_called
+
+
+# TODO: Complete me
+
 
 """
 Given 2 strings s1 and s2
@@ -17,37 +23,24 @@ s2 = tbres
 Output: 3
 Explanation: insert a at index 2, replace r with l, delete s. Just replacements
 at each index would result in 4 edits instead of 3.
-
 _______________________________________________________________________________
 
-How do we break down the problem into simpler manageable pieces? Consider 3
-cases:
+So, each mismatch could be fixed by:
+    - Deletion
+    - Insert
+    - Replace
 
-1. 
-    s1 = table
-    s2 = tgable
-    
-    required operation - Delete
-    
-2. 
-    s1 = table
-    s2 = tble
-    
-    required operation - Insert
-
-3. 
-    s1 = table
-    s2 = tcble
-    
-    required operation - Replace
-    
-^ are 3 simple (manageable) subproblems, which we know how to solve. 
 
 """
 
 
+Cache = t.MutableMapping[tuple[int, int], int]
+
+
 @times_called
-def find_min_operations(s1: str, s2: str, index1: int, index2: int) -> int:
+def find_min_operations(
+    s1: str, s2: str, index1: int, index2: int, cache: Cache
+) -> int:
     # Base cases - end of one of the strings
     if index1 == len(s1):
         return len(s2) - index2
@@ -56,18 +49,22 @@ def find_min_operations(s1: str, s2: str, index1: int, index2: int) -> int:
 
     # If both characters match, consider the next pair
     if s1[index1] == s2[index2]:
-        return find_min_operations(s1, s2, index1 + 1, index2 + 1)
+        return find_min_operations(s1, s2, index1 + 1, index2 + 1, cache)
 
     # Haven't reached the end and chars do not match, find what to do
     # by considering 3 subproblems and picking the one needs the fewest edits
-    delete = 1 + find_min_operations(s1, s2, index1, index2 + 1)
-    insert = 1 + find_min_operations(s1, s2, index1 + 1, index2)
-    replace = 1 + find_min_operations(s1, s2, index1 + 1, index2 + 1)
-    return min(delete, insert, replace)
+    key = (index1, index2)
+    if key not in cache:
+        delete = 1 + find_min_operations(s1, s2, index1, index2 + 1, cache)
+        insert = 1 + find_min_operations(s1, s2, index1 + 1, index2, cache)
+        replace = 1 + find_min_operations(s1, s2, index1 + 1, index2 + 1, cache)
+        cache[key] = min(delete, insert, replace)
+
+    return cache[key]
 
 
 def main():
-    print(find_min_operations("table", "tbrles", 0, 0))
+    print(find_min_operations("table", "tbrles", 0, 0, {}))
 
 
 if __name__ == '__main__':
