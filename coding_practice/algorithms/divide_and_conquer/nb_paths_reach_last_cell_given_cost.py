797ca3629c4ac9963fdb4cdfbf5cba5e39348811
Evgenii Titov <1994etitov@gmail.com>
2022-08-08 10:09:00 +1000

    ...

--- coding_practice/algorithms/divide_and_conquer/nb_paths_reach_last_cell_given_cost.py
+++ coding_practice/algorithms/divide_and_conquer/nb_paths_reach_last_cell_given_cost.py
@@ -9,7 +9,11 @@ Each cell has a cost associated with it for accessing
 Starting from 0,0 need to reach n-1,m-1
 We can only go down and right
 We're given the total cost to reach the last cell
-Find the total N of paths to reach the end within the given total cost
+Find the total N of paths to reach the end with the given total cost (=)
+
+Base cases:
+    - Out of bounds
+    - Ran out of cost
 """
 
 
@@ -20,20 +24,17 @@ def find_paths(
     matrix: Matrix, cost: int, i: int = 0, j: int = 0
 ) -> t.Union[int, float]:
     # Base cases
+    rows = len(matrix)
+    cols = len(matrix[0])
+    if i >= rows or j >= cols:
+        return 0
+
     remaining_cost = cost - matrix[i][j]
     if remaining_cost < 0:
         return 0
 
-    rows = len(matrix)
-    cols = len(matrix[0])
-
     if i == rows - 1 and j == cols - 1:
         return 1 if remaining_cost == 0 else 0
-
-    if i == rows - 1:
-        return find_paths(matrix, remaining_cost, i, j + 1)
-    elif j == cols - 1:
-        return find_paths(matrix, remaining_cost, i + 1, j)
 
     path1 = find_paths(matrix, remaining_cost, i + 1, j)
     path2 = find_paths(matrix, remaining_cost, i, j + 1)
