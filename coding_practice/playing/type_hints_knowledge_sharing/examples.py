dc457c87c59128270482591457fd0a861cf0ec1b
Evgenii Titov <1994etitov@gmail.com>
2022-08-10 11:04:35 +1000

    ...

--- coding_practice/playing/type_hints_knowledge_sharing/examples.py
+++ coding_practice/playing/type_hints_knowledge_sharing/examples.py
@@ -13,11 +13,17 @@ from typing import (
     Any,
     Sequence,
     Iterable,
-    Literal
+    Literal,
+    MutableMapping,
+    Sized,
+    TypeVar,
+    Generic,
+    Protocol
 )
 import os
 from queue import Queue
 from dataclasses import dataclass
+from logging import Logger
 
 import numpy as np
 import pandas as pd
@@ -60,7 +66,15 @@ class Logger:
         print(f"Logger {self._name} -- {text}")
 
 
-def process_files(folder: str, logger: Optional[Logger] = None) -> None:
+# def process_files(folder: str, logger: Optional[Logger] = None) -> None:
+#     for filepath in get_file_paths(folder):
+#         if logger:
+#             logger.info(f"Processing file: {filepath}")
+#         else:
+#             print(f"Processing file: {filepath}")
+
+
+def process_files(folder: str, logger: Logger | None = None) -> None:
     for filepath in get_file_paths(folder):
         if logger:
             logger.info(f"Processing file: {filepath}")
@@ -75,7 +89,7 @@ def get_file_paths(folder: str) -> Iterator[str]:
 
 
 # ------------------ Alias ------------------
-GPUMetrics = List[Mapping[str, Any]]
+GPUMetrics = List[MutableMapping[str, Any]]
 
 
 Batch = list[np.ndarray]
@@ -149,6 +163,18 @@ def extend_metrics(metrics: Mapping[str, Any]) -> None:
     metrics["ABC"] = 123
 
 
+class CustomObject:
+    def __init__(self, name: str):
+        self.name = name
+
+    def __len__(self):
+        return 1
+
+
+def show_length(obj: Sized) -> int:
+    return len(obj)
+
+
 # ------------------ Literal ------------------
 
 
@@ -158,6 +184,92 @@ def upload_df_to_bq(
     print("Uploading df with mode:", mode)
 
     print(df.columns)
+
+
+# ------------------ Generics ------------------
+
+T = TypeVar("T")
+
+
+def get_first(items: Sequence[T]) -> T:
+    return items[0]
+
+
+class LoggedVar(Generic[T]):
+    def __init__(self, value: T, name: str, logger: Logger) -> None:
+        self.name = name
+        self.logger = logger
+        self.value = value
+
+    def set(self, new: T) -> None:
+        self.log('Set ' + repr(self.value))
+        self.value = new
+
+    def get(self) -> T:
+        self.log('Get ' + repr(self.value))
+        return self.value
+
+    def log(self, message: str) -> None:
+        self.logger.info('%s: %s', self.name, message)
+
+
+def zero_all_vars(vars: Iterable[LoggedVar[int]]) -> None:
+    for var in vars:
+        var.set(0)
+
+#
+# T = TypeVar('T')  # Can be anything
+# S = TypeVar('S', bound=str)  # Can be any subtype of str
+# A = TypeVar('A', str, bytes)  # Must be exactly str or bytes
+#
+#
+# def repeat(x: T, n: int) -> Sequence[T]:
+#     """Return a list containing n references to x."""
+#     return [x]*n
+#
+#
+# def print_capitalized(x: S) -> S:
+#     """Print x capitalized, and return x."""
+#     print(x.capitalize())
+#     return x
+#
+#
+# def concatenate(x: A, y: A) -> A:
+#     """Add two strings or bytes objects together."""
+#     return x + y
+
+
+# ------------------ Custom objects and Protocols ------------------
+
+
+class BaseModel(Protocol):
+
+    def predict(self, df: pd.DataFrame) -> Any:
+        ...
+
+
+class KNNModel(BaseModel):
+
+    def predict(self, df: pd.DataFrame) -> Any:
+        print("KNN predicting...")
+
+
+class SVMModel(BaseModel):
+
+    def predict(self, df: pd.DataFrame) -> Any:
+        print("SVM predicting...")
+
+
+class RandomModel:
+
+    def score(self, df: pd.DataFrame, range: tuple[int, int]) -> float:
+        print("RandomModel predicting")
+        return 0.0
+
+
+def run_model(data: pd.DataFrame, model: BaseModel):
+    model.predict(data)
+
 
 
 def main():
@@ -185,9 +297,27 @@ def main():
 
     # count()
 
+    # df = pd.DataFrame()
+    # upload_df_to_bq(df, mode="APPEND")
+    # upload_df_to_bq(df, mode="RANDOM")
+
+    # show_length(CustomObject("name"))
+
     df = pd.DataFrame()
-    upload_df_to_bq(df, mode="APPEND")
-    upload_df_to_bq(df, mode="RANDOM")
+    model = KNNModel()
+    run_model(df, model)
+
+    random_model = RandomModel()
+    run_model(df, random_model)
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
